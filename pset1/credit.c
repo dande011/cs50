00fd1c5fb8ab6845095ec20aa59ef458a43e7939
David Anderson <jonie4@gmail.com>
2017-06-09 00:15:49 -0700

    Finished credit

--- pset1/credit.c
+++ pset1/credit.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
-
 int main(){
 	char number[80];
-	// scanf("%s",number);
-	printf("%s",number);
-	// check formatting
-	// mult every other digit by 2
-	// sum all digits
-	
+	const char* card[11] = {0};
+	char invalid[8] = "INVALID";
+	int length, sumOddDigit, sumEvenDigit, ok, retry = {0};
+	sumEvenDigit = 0;
+	printf("Number: ");
+	do{
+		if(retry){printf("Retry: ");}
+		scanf("%s%n",number,&length);
+		ok = true;
+		for(int i=0; i<=length-1; i++){
+			if((number[i] = number[i]-48) > 9){ok = false; retry = true;}}
+	}while(!ok);
+	if(number[0]==4 && length==13 || (length == 16)){*card = "VISA";}
+	else if( (number[0]==3) && ((number[1]==4) || (number[1]==7)))
+		*card = "AMEX";
+	else if((number[0]==5) && (1<=number[1]<=5)){*card = "MASTERCARD";}
+	for(int i=length-1; i>=0; i-=2){sumOddDigit += number[i];}
+	for(int i=length; i>0; i-=2){
+		sumEvenDigit += ((number[i]*2>9)?number[i]*2-9:number[i]*2);}
+	printf("%s\n",( ((sumEvenDigit + sumOddDigit)%10)?invalid:*card));
 }
